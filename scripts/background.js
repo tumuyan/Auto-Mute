769a580c056fc1d374f7ecac64a2787bc72e93b6
tumuyan <tumuyan@gmail.com>
2022-11-05 17:54:45 +0800

    升级到manifest v3

--- scripts/background.js
+++ scripts/background.js
@@ -1,59 +1,96 @@
+// 正在播放声音的标签
 var tabNow = -1;
+// 前台标签
 var tabActive = -1;
 var sw_on = true;
-
-if (localStorage['sw_on'] == 'false')
-    sw_on = false;
-sw(sw_on);
-
 var sw_audible = true;
-if (localStorage['audible'] == 'false')
-    sw_audible = false;
-sw2(sw_audible);
+var options = {};
+chrome.storage.local.get(null, (data) => {
+    options = data
+    sw_on = options.autoMute != false;
+    sw_audible = options.onlyAudible != false;
 
-chrome.tabs.onActivated.addListener(function (activeInfo) {
+    options.onlyAudible = sw_audible;
+    options.autoMute = sw_on;
+    chrome.storage.local.set(options, function () { });
+});
+
+chrome.storage.onChanged.addListener((changes, area) => {
+    if (area === 'local') {
+        if (changes.autoMute != undefined) {
+            options.autoMute = changes.autoMute.newValue;
+            sw_on = options.autoMute;
+
+            if (sw_on) {
+                muteAll(true);
+
+                chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tab) {
+                    tabNow = tab.id;
+                    chrome.tabs.update(tab.id, { muted: false });
+                });
+            }
+        }
+        if (changes.onlyAudible != undefined) {
+            options.onlyAudible = changes.onlyAudible.newValue;
+            sw_audible = options.onlyAudible;
+        }
+        console.log("storage.onChanged new:");
+        console.log(changes);
+    }
+});
+
+
+async function insertScript(activeInfo) {
+    console.log("onSelectionChanged: " + tabActive + " -> " + activeInfo.tabId + ", autoMute=" + options.autoMute);
+
     tabActive = activeInfo.tabId;
-    console.log("onSelectionChanged: tabid-> " + activeInfo.tabId + " sw_on=" + sw_on);
-    if (!sw_on)
-        return;
 
     try {
         let tabId = activeInfo.tabId;
-        chrome.tabs.get(tabId, async (tab) => {
+        const tab = await chrome.tabs.get(tabId);
+        let muted = tab.active ? false : true;
+        await chrome.tabs.update(tabId, { muted: muted });
 
-            let muted = tab.active ? false : true;
-            await chrome.tabs.update(tabId, { muted: muted });
-
-            // 不智能跳过，或者智能跳过但是这个页面发音
-            if (!sw_audible || tab.audible) {
-                if (tabNow != tabId) {
+        // 不智能跳过，或者智能跳过但是这个页面发音
+        if (!sw_audible || tab.audible) {
+            if (tabNow != tabId) {
+                if (tabNow > 0)
                     await chrome.tabs.update(tabNow, { muted: true });
-                    tabNow = tabId;
-                }
+                tabNow = tabId;
             }
-            console.log(`Tab ${tab.id} is ${muted ? 'muted' : 'unmuted'}  audiable=${tab.audible}`);
-        });
+        }
+        // console.log(`Tab ${tab.id} is ${muted ? 'muted' : 'unmuted'}  audiable=${tab.audible}`);
 
     } catch (e) {
         console.log("Error-Mute-Tab-" + tabNow + "\n" + e);
     }
 
+}
+
+chrome.tabs.onActivated.addListener(function (tab) {
+    if (!sw_on) {
+        console.log("onSelectionChanged: " + tab.tabId + " exit, autoMute=" + options.autoMute);
+        return;
+    }
+
+    setTimeout(function () {
+        insertScript(tab);
+    }, 0);
 });
 
 
-// 如果智能跳过，需要检查前台标签内容更新时自动发声
-chrome.tabs.onUpdated.addListener(function (id, info, tab) {
-    if (sw_audible && id == tabActive && id != tabNow && tab.audible) {
-        chrome.tabs.update(id, { muted: false });
-        chrome.tabs.update(tabNow, { muted: true });
+// 如果智能跳过，需要检查前台标签内容更新时自动发声; 否则切换标签页时已经处理
+chrome.tabs.onUpdated.addListener(async function (id, info, tab) {
+    if (options.onlyAudible && id == tabActive && id != tabNow && tab.audible) {
+        await chrome.tabs.update(id, { muted: false });
+        if (tabNow > 0)
+            await chrome.tabs.update(tabNow, { muted: true });
         tabNow = tabActive;
     }
 });
-
+/* 
 function sw(sw) {
     sw_on = sw;
-    localStorage['sw_on'] = sw_on;
-
     if (sw_on) {
         muteAll(true);
 
@@ -62,11 +99,13 @@ function sw(sw) {
             chrome.tabs.update(tab.id, { muted: false });
         });
     }
+
+    options.autoMute = sw_on;
+    chrome.storage.local.set(options, function () { });
 }
 
 function sw2(sw) {
     sw_audible = sw;
-    localStorage['audible'] = sw_audible;
 
     if (sw_audible) {
         muteAll(true);
@@ -75,9 +114,13 @@ function sw2(sw) {
             tabNow = tab.id;
             chrome.tabs.update(tab.id, { muted: false });
         });
+
+
+        options.onlyAudible = sw_audible;
+        chrome.storage.local.set(options, function () { });
     }
 }
-
+ */
 function muteAll(mute) {
     chrome.windows.getAll({ populate: true }, function (windows) {
         windows.forEach(function (window) {
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,99 +1,103 @@
-var sw_on = true;
-if (localStorage['sw_on'] == 'false')
-    sw_on = false;
-
-var sw_audible = true;
-if (localStorage['audible'] == 'false')
-    sw_audible = false;
+// 静音/恢复当前标签
+var btnMuteActive
+var options = {};
 
 let queryActiveOptions = { active: true, lastFocusedWindow: true };
 
 document.addEventListener('DOMContentLoaded', function () {
 
     // 自动静音
-    var auto = document.getElementById('auto');
-    auto.textContent = chrome.i18n.getMessage("autoMute");
+    var btnAuto = document.getElementById('auto');
+    btnAuto.textContent = chrome.i18n.getMessage("autoMute");
 
     // 静音全部
-    var mute = document.getElementById('mute');
-    mute.textContent = chrome.i18n.getMessage("allMute");
+    var btnMute = document.getElementById('mute');
+    btnMute.textContent = chrome.i18n.getMessage("allMute");
 
     // 恢复全部
-    var unmute = document.getElementById('unmute');
-    unmute.textContent = chrome.i18n.getMessage("unMute");
+    var btnUnMute = document.getElementById('unmute');
+    btnUnMute.textContent = chrome.i18n.getMessage("unMute");
 
     // 跳过不发音标签
-    var audible = document.getElementById('audible');
-    audible.textContent = chrome.i18n.getMessage("audible");
+    var btmOnlyAudible = document.getElementById('audible');
+    btmOnlyAudible.textContent = chrome.i18n.getMessage("audible");
 
     // 静音/恢复当前标签
-    var muteactive = document.getElementById('muteactive');
-    muteactive.textContent = chrome.i18n.getMessage("unmuteactive");
+    btnMuteActive = document.getElementById('muteactive');
+    btnMuteActive.textContent = chrome.i18n.getMessage("unmuteactive");
 
-    chrome.tabs.query(queryActiveOptions, ([tab]) => {
-        if (chrome.runtime.lastError)
-            console.error(chrome.runtime.lastError);
+    chrome.storage.local.get(null, async (data) => {
+        options = data;
+        console.log(options);
 
-        console.log(tab.mutedInfo);
-        let activeMute = tab.mutedInfo.muted;
-        if (activeMute)
-            muteactive.setAttribute("sw", "off");
-        else
-            muteactive.setAttribute("sw", "on");
+        if (options.autoMute) {
+            btnAuto.setAttribute("sw", "on");
+        } else {
+            btnAuto.setAttribute("sw", "off");
+        }
+
+        if (options.onlyAudible) {
+            btmOnlyAudible.setAttribute('sw', "on");
+        } else {
+            btmOnlyAudible.setAttribute("sw", "off");
+        }
+
+        {
+            let [tab] = await chrome.tabs.query(queryActiveOptions);
+            if (tab == undefined) {
+                console.log("active tab = null")
+            } else {
+                console.log(tab)
+                let activeMute = tab.mutedInfo.muted;
+                if (activeMute == true)
+                    btnMuteActive.setAttribute("sw", "off");
+                else
+                    btnMuteActive.setAttribute("sw", "on");
+            }
+        }
+
     });
 
 
-    let bg = chrome.extension.getBackgroundPage();
-
-    if (sw_on) {
-        auto.setAttribute("sw", "on");
-    } else {
-        auto.setAttribute("sw", "off");
-    }
-
-    if (sw_audible) {
-        audible.setAttribute('sw', "on");
-    } else {
-        audible.setAttribute("sw", "off");
-    }
-
-    auto.onclick = function () {
-        sw_on = autoMute(!sw_on);
-        if (sw_on) {
-            auto.setAttribute("sw", "on");
+    btnAuto.onclick = function () {
+        options.autoMute = !options.autoMute;
+        if (options.autoMute) {
+            btnAuto.setAttribute("sw", "on");
         } else {
-            auto.setAttribute("sw", "off");
+            btnAuto.setAttribute("sw", "off");
         }
+        chrome.storage.local.set(options, function () { });
     }
 
 
-    audible.onclick = function () {
-        sw_audible = !sw_audible;
+    btmOnlyAudible.onclick = function () {
+        options.onlyAudible = !options.onlyAudible;
 
-        var bg = chrome.extension.getBackgroundPage();
-        bg.sw2(sw_audible);
-
-        if (sw_audible) {
-            audible.setAttribute('sw', "on");
+        if (options.onlyAudible) {
+            btmOnlyAudible.setAttribute('sw', "on");
         } else {
-            audible.setAttribute("sw", "off");
+            btmOnlyAudible.setAttribute("sw", "off");
         }
+
+        chrome.storage.local.set(options, function () { });
     }
 
-    mute.onclick = function () {
+    btnMute.onclick = function () {
         console.log('mute');
         muteAll(true);
     }
 
-    unmute.onclick = function () {
+    btnUnMute.onclick = function () {
         console.log('unute');
         muteAll(false);
 
-        sw_on = autoMute(false)
-        auto.setAttribute("sw", "off");
+        options.autoMute = false;
+        btnAuto.setAttribute("sw", "off");
+
+        chrome.storage.local.set(options, function () { });
     }
 
-    muteactive.onclick = function () {
+    btnMuteActive.onclick = function () {
 
         chrome.tabs.query(queryActiveOptions, ([tab]) => {
             if (chrome.runtime.lastError)
@@ -103,9 +107,9 @@ document.addEventListener('DOMContentLoaded', function () {
             let activeMute = !tab.mutedInfo.muted;
             chrome.tabs.update(tab.id, { muted: activeMute });
             if (activeMute)
-                muteactive.setAttribute("sw", "off");
+                btnMuteActive.setAttribute("sw", "off");
             else
-                muteactive.setAttribute("sw", "on");
+                btnMuteActive.setAttribute("sw", "on");
         });
     }
 
@@ -115,10 +119,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function muteAll(mute) {
 
-    if (!mute) {
-        var bg = chrome.extension.getBackgroundPage();
-        bg.sw(false);
-    }
+    if (mute)
+        btnMuteActive.setAttribute("sw", "off");
+    else
+        btnMuteActive.setAttribute("sw", "on");
 
     chrome.windows.getAll({ populate: true }, function (windows) {
         windows.forEach(function (window) {
@@ -129,10 +133,3 @@ function muteAll(mute) {
     });
 }
 
-function autoMute(on) {
-    sw_on = on;
-    var bg = chrome.extension.getBackgroundPage();
-    bg.sw(sw_on);
-    console.log('autoMute: ' + sw_on);
-    return sw_on;
-}
